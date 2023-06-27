2ea3f6a958654ad12370e58582a10e1abe1c69d4
Philipp Wagner <pwag86@googlemail.com>
2023-06-27 10:14:47 +0200

    Update git_indexer.ps1

--- Scripts/git_indexer.ps1
+++ Scripts/git_indexer.ps1
@@ -5,66 +5,9 @@
     
 #>
 
-function Git-CloneRepository {
-    param (
-        [Parameter(Mandatory)]
-        [string]$Url
-        
-        [Parameter(Mandatory)]
-        [string]$Directory
-    )
-    
-    git clone $repository.url $Directory
-}
-
-function Git-BuildFileList {
-    param (
-        [Parameter(Mandatory)]
-        [string]$Directory
-    )
-    
-    $files = git --git-dir "$Directory/.git" ls-files 2>&1 
-        | % {$_ -Split "`r`n"}
-    
-    return $files
-}
-
-function Git-ForceDeleteRepository {
-    param (
-        [Parameter(Mandatory)]
-        [string]$Directory
-    )
-    
-    Remove-Item -LiteralPath $Directory -Force -Recurse
-}
-
-
-function Git-LatestCommitDate {
-    param (
-        [Parameter(Mandatory)]
-        [string]$Directory,
-        
-        [Parameter(Mandatory)]
-        [string]$Filename
-    )
-    
-    $latestCommitDate = git --git-dir "$Directory/.git" log -1  --date=format-local:'%Y-%m-%d %H:%M:%S' --format="%ad" -- $File 2>&1
-    
-    return $latestCommitDate
-}
-
 # The organization (or user) we are going to index all repositories 
 # from. This should be passed as a parameter in a later version ...
 $organization = "microsoft"
-
-# The Url, where the Index Service is running at. This is the ASP.NET 
-# Core WebAPI, which is responsible to send the indexing requests ...
-$codeSearchIndexUrl = "http://localhost:5000/api/index"
-
-# This is where we are going to clone the temporary Git repositories to, 
-# which will be created for reading the file content and sending it to 
-# the ElasticSearch instance.
-$baseDirectory = "C:\Temp"
 
 # We want to index repositories with a maximum of 700 MB initially, so we 
 # filter all large directories ...
@@ -78,42 +21,96 @@ $repositories = gh repo list $Organization --json id,name,nameWithOwner,language
 # Index all files of the organization or user, cloning is going to take some time, 
 # so we should probably be able to clone and process 4 repositories in parallel. We 
 # need to check if it reduces the waiting time ...
-$repositories | ForEach-Object -parallel {
+$repositories | ForEach-Object -Parallel {
 
+    # The Url, where the Index Service is running at. This is the ASP.NET 
+    # Core WebAPI, which is responsible to send the indexing requests ...
+    $codeSearchIndexUrl = "http://localhost:5000/api/index"
+
+    # This is where we are going to clone the temporary Git repositories to, 
+    # which will be created for reading the file content and sending it to 
+    # the ElasticSearch instance.
+    $baseDirectory = "C:\Temp"
+
+    # Repository Name.
+    $repositoryName = $_.name
+    
+    # Repository URL.
+    $repositoryUrl = $_.url
+
+    # Repository URL.
+    $repositoryOwner = $_.owner
+    
+    # Repository Path.
+    $repositoryDirectory = "$baseDirectory\$repositoryName"
+                
+    Write-Host "Processing '$repositoryName' ..."
+    
     # Wrap the whole thing in a try / catch, so we always delete the repositories, 
     # when we have hit an error of are stuck.
     try {
         
-        $repositoryName = $_.name
-        
-        # Repository Path.
-        $repositoryDirectory = $baseDirectory + "\" + $repositoryName
+        if(Test-Path $repositoryDirectory) {
             
+            Write-Host "[$repositoryName] Directory '$repositoryDirectory' already exists. Deleting ...."
+            
+            #Remove-Item -LiteralPath $repositoryDirectory -Force -Recurse
+        }
+        
+        Write-Host "[$repositoryName]: Cloning to '$repositoryDirectory' ..."
+        
+        # Clone the Repository to the target directory.
+        git clone $repositoryUrl $repositoryDirectory
+        
         # Get all files in the repositrory using the GIT CLI. This command 
         # returns relative filenames starting at the Repository Path.
-        $relativeFilenames = Git-BuildFileList -Directory $repositoryDirectory
+        $relativeFilenames = git --git-dir "$repositoryDirectory/.git" ls-files 2>&1 
+            | % {$_ -Split "`r`n"}
 
+        Write-Host "[$repositoryName] '$($relativeFilenames.Length)' Files to Process ..."
+        
         # We want to create Bulk Requests, so we don't send a million
-        # Requests to the Elasticsearch API. .NET 6 now comes with a 
-        # Enumerable#Chunk to partition data, let's use it.
-        $chunks =  [System.Linq.Enumerable]::Chunk($relativeFilenames, 100)
+        # Requests to the Elasticsearch API. We will use Skip and Take, 
+        # probably not efficient, but who cares.
+        $chunks = @()
+        $chunkSize = 20
+        
+        for($i = 0; $i -lt $relativeFilenames.Length; $i += $chunkSize) {
+            Write-Host "Building Chunk (Current Element $i, ChunkSize $chunkSize)"
+            
+            $batch = @()
+            
+            $batch = $relativeFilenames
+                | Select-Object -Skip $i
+                | Select-Object -First $chunkSize
+                
+            $chunks += , $batch
+        }
+        
+        Write-Host "[$repositoryName] '$($chunks.Length)' Bulk Index Requests will be sent to Indexing Service ..."
         
         # Process all File Chunks in Parallel. This allows us to send 
         # Bulk Requests to the Elasticsearch API, without complex code 
         # ...
-        $chunks | ForEach-Object {  
+        $chunks | ForEach-Object -Parallel {  
 
+
+            $codeSearchIndexUrl = $using:codeSearchIndexUrl
+            $repositoryDirectory = $using:repositoryDirectory
+            $repositoryOwner = $using:repositoryOwner
+            $repositoryName = $using:repositoryName
+            
             # Holds the File Index Data, that we are going to send 
             # to Elasticsearch for indexing.
             $codeSearchDocumentList = @()
-            
+                        
             # Each Chunk contains a list of files.
-            foreach($relativeFilename ile in $_) {
+            foreach($relativeFilename in $_) {
                 
                 # We need the absolute filename for the Powershell Utility functions,
                 # so we concatenate the path to the repository with the relative filename 
                 # as returned by git.
-                $absoluteFilename = $repositoryDirectory + "\" + $relativeFilename
+                $absoluteFilename = "{0}\{1}" -f $repositoryDirectory,$relativeFilename
                 
                 # Read the Content, that should be indexed, we may 
                 # exceed memory limits, on very large files, but who 
@@ -128,26 +125,28 @@ $repositories | ForEach-Object -parallel {
                 # Get the latest Commit Date from the File, so we 
                 # can later sort by commit date, which is the only 
                 # reason for building this thing...
-                $latestCommitDate = Get-LatestCommitDate -Directory $repositoryDirectory -Filename $relativeFilename
+                $latestCommitDate = git --git-dir "$repositoryDirectory\.git" log -1  --date=format-local:'%Y-%m-%d %H:%M:%S' --format="%ad" -- $relativeFilename 2>&1
+                
+                Write-Host $latestCommitDate
                 
                 # This is the 
                 $codeSearchDocument = @{
                     Id = $md5Hash
-                    Owner = $owner
-                    Repository = $repository
+                    Owner = $repositoryOwner
+                    Repository = $repositoryName
                     Filename = $relativeFilename
                     Content = $content
                     LatestCommitDate = $LatestCommitDate
                 }
-                
-                $codeSearchDocumentList += $codeSearchDocument
+                                
+                $codeSearchDocumentList += , $codeSearchDocument
             }
             
             # Build the actual HTTP Request.
             $codeSearchIndexRequest = @{
                 Method = "POST"
                 Uri = $codeSearchIndexUrl
-                Body = ($codeSearchDocumentList | Convert-ToJson)
+                Body = ($codeSearchDocumentList | ConvertTo-Json)
                 ContentType = "application/json"
                 StatusCodeVariable = 'statusCode'
             }
@@ -155,19 +154,24 @@ $repositories | ForEach-Object -parallel {
             Write-Host "[REQ]"
             Write-Host "[REQ] Code Search Index Request"
             Write-Host "[REQ]"
-            Write-Host "[REQ]   URL:            $indexServiceUrl"
+            Write-Host "[REQ]   URL:            $codeSearchIndexUrl"
             Write-Host "[REQ]   File Count:     $($codeSearchDocumentList.Length)"
             Write-Host "[REQ]"
 
-            # And Invoke it
-            $codeSearchIndexResponse = Invoke-RestMethod @codeSearchIndexRequest
-                       
-            Write-Host "[RES]    HTTP Status:    $statusCode"  -ForegroundColor Green
+            try {
+                # And Invoke it
+                $codeSearchIndexResponse = Invoke-RestMethod @codeSearchIndexRequest
+                           
+                Write-Host "[RES]    HTTP Status:    $statusCode"  -ForegroundColor Green
+            } catch {
+                Write-Host "[ERR] Request failed with Error Message: " $_.Exception.Message -ForegroundColor Red
+            }
         
-        } -ThrottleLimit 25
+        } -ThrottleLimit 1
     }
     finally {
-        Git-ForceDeleteRepository -Directory $repository
+        Write-Host "Deleting GIT Repository: $repositoryDirectory ..."
+        
+        #Remove-Item -LiteralPath $repositoryDirectory -Force -Recurse
     }
-    
-} -ThrottleLimit 4
+} -ThrottleLimit 1
