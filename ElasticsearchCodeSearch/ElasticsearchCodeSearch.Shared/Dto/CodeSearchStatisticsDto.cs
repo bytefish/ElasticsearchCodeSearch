78c41ddcf3120fc504f5e29348bec3279dd04a3e
Philipp Wagner <pwag86@googlemail.com>
2023-08-10 07:53:48 +0200

    Add License Headers, StatisticsConverter for Metrics added

---
+++ ElasticsearchCodeSearch/ElasticsearchCodeSearch.Shared/Dto/CodeSearchStatisticsDto.cs
@@ -0,0 +1,71 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace ElasticsearchCodeSearch.Shared.Dto
+{
+    /// <summary>
+    /// 
+    /// Aggregates Statistics from the Elasticsearch Statistics Endpoints.
+    /// 
+    /// There is a great guide for Elasticsearch monitoring and statistics by the DataDog team:
+    /// 
+    ///     https://www.datadoghq.com/blog/monitor-elasticsearch-performance-metrics/#search-performance-metrics
+    ///     
+    /// </summary>
+    public class CodeSearchStatisticsDto
+    {
+        /// <summary>
+        /// Index Name.
+        /// </summary>
+        public required string IndexName { get; set; }
+
+        /// <summary>
+        /// Total Index Size in bytes (indices.total.store.size_in_bytes).
+        /// </summary>
+        public required long? IndexSizeInBytes { get; set; }
+
+        /// <summary>
+        /// Total number of documents indexed (indices.indexing.index_total).
+        /// </summary>
+        public required long? TotalNumberOfDocumentsIndexed { get; set; }
+
+        /// <summary>
+        /// Total time spent indexing documents (indices.indexing.index_time_in_millis).
+        /// </summary>
+        public required long? TotalTimeSpentIndexingDocumentsInMilliseconds { get; set; }
+
+        /// <summary>
+        /// Number of documents currently being indexed (indices.indexing.index_current).
+        /// </summary>
+        public required long? NumberOfDocumentsCurrentlyBeingIndexed { get; set; }
+                
+        /// <summary>
+        /// Total number of queries (indices.search.query_total).
+        /// </summary>
+        public required long? TotalNumberOfQueries { get; set; }
+
+        /// <summary>
+        /// Total time spent on queries (indices.search.query_time_in_millis).
+        /// </summary>
+        public required long? TotalTimeSpentOnQueriesInMilliseconds { get; set; }
+
+        /// <summary>
+        /// Number of queries currently in progress (indices.search.query_current).
+        /// </summary>
+        public required long? NumberOfQueriesCurrentlyInProgress { get; set; }
+
+        /// <summary>
+        /// Total number of fetches (indices.search.fetch_total).
+        /// </summary>
+        public required long? TotalNumberOfFetches { get; set; }
+
+        /// <summary>
+        /// Total time spent on fetches (indices.search.fetch_time_in_millis).
+        /// </summary>
+        public required long? TotalTimeSpentOnFetchesInMilliseconds { get; set; }
+
+        /// <summary>
+        /// Number of fetches currently in progress (indices.search.fetch_current).
+        /// </summary>
+        public required long? NumberOfFetchesCurrentlyInProgress { get; set; }
+    }
+}
---
+++ ElasticsearchCodeSearch/ElasticsearchCodeSearch/Converters/CodeSearchStatisticsConverter.cs
@@ -0,0 +1,41 @@
+﻿// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Elastic.Clients.Elasticsearch.IndexManagement;
+using ElasticsearchCodeSearch.Shared.Dto;
+
+namespace ElasticsearchCodeSearch.Converters
+{
+    public static class CodeSearchStatisticsConverter
+    {
+        public static CodeSearchStatisticsDto Convert(IndicesStatsResponse indicesStatsResponse)
+        {
+            if(indicesStatsResponse.Indices == null)
+            {
+                throw new Exception("No statistics available");
+            }
+
+            if(indicesStatsResponse.Indices.Count != 1)
+            {
+                throw new Exception($"Expected '1' Index, but got '{indicesStatsResponse.Indices.Count}'");
+            }
+
+            var indexName = indicesStatsResponse.Indices.First().Key;
+            var indexStats = indicesStatsResponse.Indices.First().Value;
+
+            return new CodeSearchStatisticsDto
+            {
+                IndexName = indexName,
+                IndexSizeInBytes = indexStats.Total?.Store?.SizeInBytes,
+                TotalNumberOfDocumentsIndexed = indexStats.Total?.Indexing?.IndexTotal,
+                NumberOfDocumentsCurrentlyBeingIndexed = indexStats.Total?.Indexing?.IndexCurrent,
+                TotalNumberOfFetches = indexStats.Total?.Search?.FetchTotal,
+                NumberOfFetchesCurrentlyInProgress = indexStats.Total?.Search?.FetchCurrent,
+                TotalNumberOfQueries = indexStats.Total?.Search?.QueryTotal,
+                NumberOfQueriesCurrentlyInProgress = indexStats.Total?.Search?.QueryCurrent,
+                TotalTimeSpentIndexingDocumentsInMilliseconds = indexStats.Total?.Indexing?.IndexTimeInMillis,
+                TotalTimeSpentOnFetchesInMilliseconds = indexStats.Total?.Search?.FetchTimeInMillis,
+                TotalTimeSpentOnQueriesInMilliseconds = indexStats.Total?.Search?.QueryTimeInMillis,
+            };
+        }
+    }
+}
