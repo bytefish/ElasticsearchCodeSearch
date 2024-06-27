88894efa3f8eae9a7c501fc5e05339e92a38b963
Philipp Wagner <philipp@bytefish.de>
2024-06-27 13:29:00 +0800

    Update GitExecutor.cs

--- src/ElasticsearchCodeSearch/Git/GitExecutor.cs
+++ src/ElasticsearchCodeSearch/Git/GitExecutor.cs
@@ -4,7 +4,6 @@ using ElasticsearchCodeSearch.Git.Exceptions;
 using ElasticsearchCodeSearch.Shared.Logging;
 using System.Diagnostics;
 using System.Text;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ElasticsearchCodeSearch.Git
 {
